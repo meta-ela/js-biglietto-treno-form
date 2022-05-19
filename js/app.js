7e3103cbd08e96a48b2ee0932f7d43241f1edcd1
meta-ela <anxhelameta@live.it>
2022-05-19 21:35:44 +0200

    Adds display none and variables corrections

--- js/app.js
+++ js/app.js
@@ -1,30 +1,50 @@
-const inputName = document.querySelector("[name='nome-utente']");
-const inputKm = document.querySelector("[name='km-utente']");
-const inputEta = document.querySelector("[name='eta-utente']");
 
-const prezzoBase = 0.21;
-let prezzoBigliettoBase = parseInt(inputKm) * prezzoBase;
-let prezzoBiglietto = prezzoBigliettoBase;
-prezzoBiglietto = prezzoBiglietto.toFixed(2);
+// console.log("prezzo originale:", prezzoBiglietto);
 
-console.log("prezzo originale:", prezzoBiglietto);
+let submitButtonUser = document.getElementById("submitButton")
 
-const submitButton = document.getElementById("submitButton")
+submitButtonUser.addEventListener("click", function () {
+    let inputName = document.querySelector(".nome-utente");
+    let inputKm = document.querySelector(".km-utente");
+    let inputEta = document.querySelector(".eta-utente");
+    // console.log(inputKm);
+    // console.log(inputKm.value);
 
-submitButton.addEventListener("click", function () {
-    console.log("nome utente:", inputName.value);
-    console.log("km utente:", inputKm.value);
-    console.log("età utente:", inputEta.value);
+    let prezzoBase = 0.21;
+    let kmValue = inputKm.value;
+    let nameValue = inputName.value;
+    let etaValue = inputEta.value;
+    let prezzoBigliettoBase = parseInt(kmValue) * prezzoBase;
+    let prezzoBiglietto = prezzoBigliettoBase;
+    
+
+    console.log("nome utente:", nameValue);
+    console.log("km utente:", kmValue);
+    console.log("età utente:", etaValue);
+
+
+    if (isNaN(kmValue) || isNaN(etaValue)) {
+        alert("Dati non validi");
+    } else { 
+        let displayTicket = document.getElementById("display-ticket")
+        displayTicket.className = "show"
+
+        if (parseInt(etaValue) < 18) {
+            prezzoBiglietto = prezzoBigliettoBase - (prezzoBigliettoBase * 20) / 100;
+        } else if (parseInt(etaValue) > 65) {
+            prezzoBiglietto = prezzoBigliettoBase - (prezzoBigliettoBase * 40) / 100;
+        }
+        
+        prezzoBiglietto = prezzoBiglietto.toFixed(2);
+    
+        let divPrezzoUtente = document.getElementById("prezzoUtente");
+        divPrezzoUtente.innerHTML = `${prezzoBiglietto} €`;
+        
+        // console.log("prezzo scontato:", prezzoBiglietto);
+    
+        let divNomeUtente = document.querySelector(".nome-completo-utente");
+        divNomeUtente.innerHTML = `${nameValue}`;
+    }
 });
 
 
-if (inputEta < 18) {
-    prezzoBiglietto = prezzoBigliettoBase - (prezzoBigliettoBase * 20) / 100;
-} else if (inputEta > 65) {
-    prezzoBiglietto = prezzoBigliettoBase - (prezzoBigliettoBase * 40) / 100;
-}
-
-const divPrezzoUtente = document.getElementById("prezzoUtente");
-divPrezzoUtente.innerHTML = `${prezzoBiglietto} €`;
-
-console.log("prezzo scontato:", prezzoBiglietto);
